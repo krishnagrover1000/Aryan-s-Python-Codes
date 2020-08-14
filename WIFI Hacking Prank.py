7da157cee18350b13dec6256c080cdd726c533ca
krishnagrover1000 <69473094+krishnagrover1000@users.noreply.github.com>
2020-08-14 20:40:15 +0530

    WIFI Hacking Prank.py
    For Pranking People On Their WIFI

---
+++ WIFI Hacking Prank.py
@@ -0,0 +1,38 @@
+import time
+
+WIFI_ASK = input("Enter Your WIFI Name:  ").strip()
+
+print("Finding Network...")
+time.sleep(2)
+print("Network Server Found!!")
+time.sleep(2)
+print("Obtaning IP Adress")
+time.sleep(2)
+print("IP Adress Found!!")
+time.sleep(2)
+print("Trying Passwords: ")
+time.sleep(1)
+print("210895")
+time.sleep(1)
+print("152438")
+time.sleep(1)
+print("1234567890")
+time.sleep(1)
+print("0987654321")
+time.sleep(1)
+print("09876543")
+time.sleep(2)
+print("Password Is: ")
+time.sleep(5)
+print("It Burns When IP Vankap")
+
+print("Process Done!!!")
+time.sleep(3)
+
+print("Calling Police!!!")
+
+print()
+time.sleep(5)
+print("You Got Pranked")
+
+
